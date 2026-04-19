a00f7311f700fcd1a9ec205a079392e0ede825ed
PHURI001 <phuriwat.chat@gmail.com>
2026-04-19 08:02:59 +0700

    Block

---
+++ Assets/Script/Block/Wind.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    private enum WindDirection { Up, Down, Left, Right }
+    [SerializeField] private WindDirection direction;
+
+#warning Please do this when you wake up
+}
